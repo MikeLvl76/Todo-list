c52345e06c7e85f694cc4b71174d288ca2e5715f
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2023-03-02 13:08:15 +0100

    added modal for new todo (input + button for confirmation)

--- todo-list/src/components/App.js
+++ todo-list/src/components/App.js
@@ -1,4 +1,17 @@
+import { useState } from "react";
+import ReactModal from "react-modal";
+
 export default function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [addInput, setAddInput] = useState("");
+
+  const insert = () => {
+    setIndex((prev) => prev + 1);
+    localStorage.setItem(`${index}`, addInput);
+    setModalIsOpen((prev) => !prev);
+  };
+
   return (
     <div>
       <div>
@@ -8,7 +21,26 @@ export default function App() {
         <input placeholder="Rechercher une tâche..." />
       </div>
       <div>
-        Liste des tâches
+        <ul>
+          <li>
+            <button onClick={() => setModalIsOpen((prev) => !prev)}>
+              Ajouter une tâche
+            </button>
+            <ReactModal
+              isOpen={modalIsOpen}
+              styles={{ width: "50%" }}
+              ariaHideApp={false}
+              onRequestClose={() => setModalIsOpen((prev) => !prev)}
+            >
+              <input
+                placeholder="Ajouter une tâche"
+                maxLength={50}
+                onChange={(e) => setAddInput(e.target.value)}
+              />
+              <button onClick={insert}>Confirmer</button>
+            </ReactModal>
+          </li>
+        </ul>
       </div>
       <div>
         <button>Supprimer tout</button>
