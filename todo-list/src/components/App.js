088e2129d2de36a1f7f246e3be7394de39c1b62b
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2023-03-02 11:27:05 +0100

    refactored main page

---
+++ todo-list/src/components/App.js
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <div>
+      <div>
+        <h1>Liste des tâches</h1>
+      </div>
+      <div>
+        <input placeholder="Rechercher une tâche..." />
+      </div>
+      <div>
+        Liste des tâches
+      </div>
+      <div>
+        <button>Supprimer tout</button>
+      </div>
+    </div>
+  );
+}
