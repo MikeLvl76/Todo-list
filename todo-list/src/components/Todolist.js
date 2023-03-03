fe4920d4225130da0e37ba7ac35750b3459fe017
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2023-03-03 13:16:37 +0100

    continued CSS

--- todo-list/src/components/Todolist.js
+++ todo-list/src/components/Todolist.js
@@ -9,19 +9,19 @@ export default function Todolist() {
   const [search, setSearch] = useState("");
 
   return (
-    <div className="mx-auto">
-      <div className="justify-center">
-        <h1 className="text-2xl text-italic text-white text-center w-fit bg-gray-900 rounded-full px-5 py-5 mt-5">Liste des tâches</h1>
+    <div className="flex flex-col">
+      <div className="self-center">
+        <h1 className="text-2xl text-italic text-white text-center w-fit bg-gray-900 rounded-full px-5 py-5 mt-5">
+          Liste des tâches
+        </h1>
       </div>
-      <div>
+      <div className="mx-auto">
         <input
+          className="border border-1 border-black px-2 py-2 rounded-lg mt-5 mb-5"
           placeholder="Rechercher une tâche..."
           onChange={(e) => setSearch(e.target.value)}
         />
       </div>
-      <button onClick={() => setModalIsOpen((prev) => !prev)}>
-        Ajouter une tâche
-      </button>
       <ReactModal
         isOpen={modalIsOpen}
         styles={{ width: "50%" }}
@@ -42,24 +42,35 @@ export default function Todolist() {
           Confirmer
         </button>
       </ReactModal>
-      <ul>
-        {todos
-          .filter((e) => (search.length > 0 ? e.text.includes(search) : e)) // improve this
-          .map((todo, i) => (
-            <li key={i}>
-              <div>
-                <p>{todo.text}</p>
-                <input
-                  type="checkbox"
-                  onClick={() => removeTodo(todo.id)}
-                  checked={false}
-                />
-              </div>
-            </li>
-          ))}
-      </ul>
-      <div>
-        <button onClick={() => removeAll()}>Supprimer tout</button>
+      <div className="mx-auto border border-1 border-black bg-gray-200 rounded-lg">
+        <ul className="mx-auto w-fit space-y-5 px-3 py-3">
+          {todos
+            .filter((e) => (search.length > 0 ? e.text.includes(search) : e)) // improve this
+            .map((todo, i) => (
+              <li
+                className="border border-1 border-black rounded-lg px-3 py-3 w-full bg-white hover:bg-black hover:text-white"
+                key={i}
+              >
+                <div className="flex flex-row ">
+                  <div className="flex-1 w-64">
+                    <p className="break-words">{todo.text}</p>
+                  </div>
+                  <div className="justify-end">
+                    <button
+                      className="w-4 h-4 border border-2 border-black rounded-full hover:border-white"
+                      onClick={() => removeTodo(todo.id)}
+                    />
+                  </div>
+                </div>
+              </li>
+            ))}
+        </ul>
+      </div>
+      <div className="mx-auto flex flex-row mt-2 space-x-5">
+        <button className="bg-green-500 text-white rounded-lg px-2 py-2" onClick={() => setModalIsOpen((prev) => !prev)}>
+          Ajouter une tâche
+        </button>
+        <button className="bg-red-500 text-white rounded-lg px-2 py-2" onClick={() => removeAll()}>Supprimer tout</button>
       </div>
     </div>
   );
